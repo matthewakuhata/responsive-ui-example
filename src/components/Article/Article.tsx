76152d9b1816a57857c614e6ad5f65d9e069bfb3
Matthew Akuhata <matthewakuhata@gmail.com>
2022-03-30 22:38:22 +1100

    Add article component

--- src/components/Article/Article.tsx
+++ src/components/Article/Article.tsx
@@ -1,8 +1,26 @@
 import React from "react";
 import "./article.css";
 
-const Article = () => {
-  return <div>Article</div>;
+type ArticleProps = {
+  imgUrl: string;
+  date: string;
+  text: string;
+};
+const Article = ({ imgUrl, date, text }: ArticleProps) => {
+  return (
+    <div className="blog-container_article">
+      <div className="blog-container_article-image">
+        <img src={imgUrl} alt="blog_image" />
+      </div>
+      <div className="blog-container_article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{text}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
+    </div>
+  );
 };
 
 export default Article;
